8d2f44acbb041b1b34483e443083b799ca80f561
Oscar <Oscar@DESKTOP-PHFOBV1>
2020-01-30 10:22:40 +0100

    empezando la animacion para la hormiga

--- src/main/java/com/mycompany/juego1/App.java
+++ src/main/java/com/mycompany/juego1/App.java
@@ -1,12 +1,15 @@
 package com.mycompany.juego1;
 
+import java.time.Duration;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -14,6 +17,13 @@ import javafx.stage.Stage;
  */
 public class App extends Application {
     int ladoImagen= 256;
+    short Hormiga1X = 0;
+    byte Hormiga1SpeedX = 3;
+    byte Hormiga1DirectionX = 1;
+    
+    short Hormiga1Y = 0;
+    byte Hormiga1SpeedY = 3;
+    byte Hormiga1DirectionY = 1;
 // for (int i = 0; i < 5; ++i) {
     @Override
     public void start(Stage stage) {
@@ -22,13 +32,46 @@ public class App extends Application {
         stage.setScene(scene);
         stage.show();
         
-        Image background = new Image(getClass().getResourceAsStream("/Images/background.png"));
-        ImageView  imageView1 = new ImageView (background);
+        Image fondo = new Image(getClass().getResourceAsStream("/Images/background.png"));
+        Image hormiga1 = new Image(getClass().getResourceAsStream("/Images/walk.png"));
+        //Image hormiga2 = new Image(getClass().getResourceAsStream("/Images/walk.png"));
+        
+        ImageView  imageView1 = new ImageView (fondo);
+        ImageView  imageView2 = new ImageView (fondo);
+        ImageView  imageView3 = new ImageView (fondo);
+        ImageView  imageView4 = new ImageView (fondo);
+        ImageView  imageView5 = new ImageView (fondo);
+        ImageView  imageView6 = new ImageView (fondo);
+        ImageView  Hormiga1 = new ImageView (hormiga1);
         root.getChildren().add(imageView1);
-        ImageView  imageView2 = new ImageView (background);
         root.getChildren().add(imageView2);
-        imageView2.setX(ladoImagen);
-        imageView2.setX(ladoImagen);
+        root.getChildren().add(imageView3);
+        root.getChildren().add(imageView4);
+        root.getChildren().add(imageView5);
+        root.getChildren().add(imageView6);
+        root.getChildren().add(Hormiga1);
+        
+        imageView1.setX(ladoImagen);
+        imageView2.setX(2* ladoImagen);
+        imageView3.setY(ladoImagen);
+        imageView4.setX(ladoImagen);
+        imageView4.setY(ladoImagen);
+        imageView6.setY(ladoImagen);
+        imageView6.setX(2*ladoImagen);
+        Hormiga1.setX(2*ladoImagen);
+        Hormiga1.setFitWidth(50);
+        Hormiga1.setFitHeight(58);
+        
+        Timeline timeline = new Timeline(
+            // 0.017 ~= 60 FPS
+            new KeyFrame(Duration.seconds(0.017), new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent ae) { 
+                    //ANIMACION DE LA HORMIGA
+                    Hormiga1.setY(Hormiga1Y);
+                    Hormiga1.setX(Hormiga1X);
+                }
+            })
+        );
     }
 
     public static void main(String[] args) {
